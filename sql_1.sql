cad25e27ce97ad8e79e290ee2d4e8719d4cdd67e
jinijini20 <jinijini301301@gamil.com>
2025-12-12 17:30:45 +0900

    Feat: sql 기초

---
+++ sql_1.sql
@@ -0,0 +1,18 @@
+show databases;
+use mysql;
+
+show tables;
+
+select * from user;
+
+show table status;
+
+use employees;
+
+select count(*) from employees;
+
+select * from employees;
+titlessalariesemployees
+
+
+
